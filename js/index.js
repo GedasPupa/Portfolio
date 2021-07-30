76214bce62b9379039dbde473b5cc11c6de5640a
GedasPupa <gedosas@gmail.com>
2021-07-30 23:40:47 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// IMPORTS:
+
+
+// CODE EXECUTION:
+
+/* HEADER start */
+/* HEADER end */
+
+/* HERO start */
+/* HERO end */
+
+/* ABOUT ME start */
+/* ABOUT ME end */
+
+/* SERVICES start */
+/* SERVICES end */
+
+/* HIRE ME start */
+/* HIRE ME end */
+
+/* EDUCATION start */
+/* EDUCATION end */
+
+/* WORKING HOURS start */
+/* WORKING HOURS end */
+
+/* MY RECENT WORK start */
+/* MY RECENT WORK end */
+
+/* OUR CLIENT start */
+/* OUR CLIENT end */
+
+/* TRUSTED BY start */
+/* TRUSTED end */
+
+/* CONTACT start */
+/* CONTACT end */
+
+/* FOOTER start */
+/* FOOTER end */
+
+
+
+
+
+
+
