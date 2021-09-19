86335882290db87c68de8f2165882888a2561d25
GedasPupa <gedosas@gmail.com>
2021-09-19 10:09:47 +0300

    slider in progress 3

--- js/components/testimonials/Testimonials.js
+++ js/components/testimonials/Testimonials.js
@@ -1,5 +1,17 @@
 class Testimonials {
+    constructor(data) {
+        this.data = data;
+    }
 
+    HTML() {
+        const src = this.data.imgPath + this.data.src;
+        return `<div class="testimonial">
+                    <img src="${src}" alt="${this.data.name} photo">
+                    <div class="name">${this.data.name}</div>
+                    <div class="company">${this.data.company}</div>
+                    <div class="message">${this.data.message}</div>
+                </div>`
+    }
 }
 
 export { Testimonials };
--- js/data/sliderData.js
+++ js/data/sliderData.js
@@ -1,15 +1,34 @@
 const sliderData = {
-    imgPath: './img/testimonials/',
+    imgPath: '/img/testimonials/',
+    controls: {
+        anglesEnabled: true,
+        dotsEnabled: true
+    },
     itemsPerView: [
         {
             minWidth: 0,
             itemsCount: 1
-        }
+        },
     ],
     list: [
-        {},
-        {},
-        {},
+        {
+            src: 'user.png',
+            company: 'Google',
+            name: 'Marc Kravitz',
+            message: '"The most well-known dummy text is the \'Lorem Ipsum\', which is said originated the 16th century. This ancient dummy text is also incomprehensible, of most European in Latin script."'
+        },
+        {
+            src: 'users.png',
+            company: 'Future',
+            name: 'Jill Forshore',
+            message: '"The most well-known dummy text is the \'Lorem Ipsum\', which is said originated the 16th century. This ancient dummy text is also incomprehensible, of most European in Latin script."'
+        },
+        {
+            src: 'user.png',
+            company: 'Best Perfo',
+            name: 'Kirk McFall',
+            message: '"The most well-known dummy text is the \'Lorem Ipsum\', which is said originated the 16th century. This ancient dummy text is also incomprehensible, of most European in Latin script."'
+        },
     ]
 }
 
