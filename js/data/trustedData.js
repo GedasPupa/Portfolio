8de8acaacb6d735e723101e8d839dcfddcdecbe3
GedasPupa <gedosas@gmail.com>
2021-09-19 12:27:36 +0300

    trusted-by

---
+++ js/data/trustedData.js
@@ -0,0 +1,32 @@
+const trustedData = {
+    path: './img/trusted-by/',
+    list: [
+        {
+            img: '1.png',
+            title: 'coffee'
+        },
+        {
+            img: '2.png',
+            title: 'fisherman'
+        },
+        {
+            img: '3.png',
+            title: 'mountainbike'
+
+        },
+        {
+            img: '4.png',
+            title: 'mountain'
+        },
+        {
+            img: '5.png',
+            title: 'chocolate'
+        },
+        {
+            img: '6.png',
+            title: 'market'
+        },
+    ]
+}
+
+export { trustedData };
